2568e864f34ef4c69b5b195442a93a2b3509ddbd
jeehb138 <64688546+jeehb138@users.noreply.github.com>
2023-12-05 12:00:17 +0900

    result.html 연결 test

--- app.py
+++ app.py
@@ -24,25 +24,25 @@ def about():
 @app.route('/skin_detect', methods=['GET', 'POST'])
 def skin_detect():
     if request.method == 'POST':
-        return render_template('result.html')
-        # im_file = request.files['file']
-        # if im_file != '':
-        #     im_bytes = im_file.read()
-        #     img = Image.open(io.BytesIO(im_bytes))
-        #
-        #     results = model(img, size=640)  # inference
-        #
-        #     results.ims  # array of original images (as np array) passed to model for inference
-        #     results.render()  # updates results.imgs with boxes and labels
-        #     for img in results.ims:  # 'JpegImageFile' -> bytes-like object
-        #         buffered = io.BytesIO()
-        #         img_base64 = Image.fromarray(img)
-        #         img_base64.save(buffered, format="JPEG")
-        #         encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
-        #             'utf-8')  # base64 encoded image with results
-        #         return render_template('result.html', img_data=encoded_img_data)
-        # else:
-        #     abort(404)
+
+        im_file = request.files['file']
+        if im_file != '':
+            im_bytes = im_file.read()
+            img = Image.open(io.BytesIO(im_bytes))
+
+            results = model(img, size=640)  # inference
+
+            results.ims  # array of original images (as np array) passed to model for inference
+            results.render()  # updates results.imgs with boxes and labels
+            for img in results.ims:  # 'JpegImageFile' -> bytes-like object
+                buffered = io.BytesIO()
+                img_base64 = Image.fromarray(img)
+                img_base64.save(buffered, format="JPEG")
+                encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
+                    'utf-8')  # base64 encoded image with results
+                return render_template('result.html', img_data=encoded_img_data)
+        else:
+            abort(404)
 
     else:
         return render_template("skin_detect.html")
