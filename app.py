f8a6aad729fb61250415451db3c7b3b46372f978
kkkimsuji <kimsooji516@gmail.com>
2023-12-12 17:24:36 +0900

    db

--- app.py
+++ app.py
@@ -3,14 +3,32 @@ import io
 import torch
 from PIL import Image
 from flask import Flask, render_template, request, abort
-#import requests, json
-
+import requests, json
+from ultralytics import YOLO
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import pandas as pd
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
-# 학습시킨 모델
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models_train/petom_weights.pt', force_reload=True)
+sqlite_db_path = 'sqlite:///DB.db'
+engine = create_engine(sqlite_db_path)
 
+
+def get_matching_data(model_result):
+    df = pd.read_sql_table('disease', engine)
+    filtered_df = df[df['id'] == model_result]
+    matching_data = filtered_df.to_dict(orient='records')
+    return matching_data
+
+
+model = YOLO("best.pt")
+
+
+# 학습시킨 모델
+# model = torch.hub.load('ultralytics/ultralytics', 'custom', path='models_train/best.pt', force_reload=True)
+# model = YOLO("models_train/best.pt")  # load a pretrained model (recommended for training)
 
 @app.route('/')
 def index():
@@ -26,90 +44,170 @@ def about():
 def skin_detect():
     if request.method == 'POST':
 
-        im_file = request.files['file']
-        if im_file != '':
-            im_bytes = im_file.read()
-            img = Image.open(io.BytesIO(im_bytes))
+        img_file = request.files['file']
+        img = Image.open(img_file)
 
-            results = model(img, size=640)  # inference
+        # RGB 형식으로 변환
+        img = img.convert("RGB")
 
-            results.ims  # array of original images (as np array) passed to model for inference
-            results.render()  # updates results.imgs with boxes and labels
-            for img in results.ims:  # 'JpegImageFile' -> bytes-like object
-                buffered = io.BytesIO()
-                img_base64 = Image.fromarray(img)
-                img_base64.save(buffered, format="JPEG")
-                encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
-                    'utf-8')  # base64 encoded image with results
-                return render_template('result.html', img_data=encoded_img_data)
-        else:
-            abort(404)
+        # 변환된 이미지를 사용하여 모델에 전달
+        results = model.predict(source=img, save=True)
+
+        model_result = results.probs.top5
+
+        matching_data = get_matching_data(model_result[0])
+
+        image_directory = 'runs/classify/predict/'
+
+        # 이미지 파일명을 동적으로 얻어오기
+        image_files = os.listdir(image_directory)
+        if not image_files:
+            return "No image files found."
+
+        # 첫 번째 이미지 파일 사용
+        dynamic_image_filename = image_files[0]
+        img_path = os.path.join(image_directory, dynamic_image_filename)
+
+        return render_template('result.html', matching_data=matching_data, img_path=img_path)
+
+        # if im_file != '':
+        #     im_bytes = im_file.read()
+        #     img = Image.open(io.BytesIO(im_bytes))
+
+        #     results = model.predict(source='static/images/result_sample_eu.png', save=True)
+
+        # results = model.predict(img)  # inference
+        #
+        # results.ims  # array of original images (as np array) passed to model for inference
+        # results.render()  # updates results.imgs with boxes and labels
+        # for img in results.ims:  # 'JpegImageFile' -> bytes-like object
+        #     buffered = io.BytesIO()
+        #     img_base64 = Image.fromarray(img)
+        #     img_base64.save(buffered, format="JPEG")
+        #     encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
+        #         'utf-8')  # base64 encoded image with results
+        #     return render_template('result.html', img_data=encoded_img_data)
+        # else:
+        #     abort(404)
 
     else:
         return render_template("skin_detect.html")
+
 
 @app.route('/eye_detect', methods=['GET', 'POST'])
 def eye_detect():
     if request.method == 'POST':
 
-        im_file = request.files['file']
-        if im_file != '':
-            im_bytes = im_file.read()
-            img = Image.open(io.BytesIO(im_bytes))
 
-            results = model(img, size=640)  # inference
+        img_file = request.files['file']
+        img = Image.open(img_file)
 
-            results.ims  # array of original images (as np array) passed to model for inference
-            results.render()  # updates results.imgs with boxes and labels
-            for img in results.ims:  # 'JpegImageFile' -> bytes-like object
-                buffered = io.BytesIO()
-                img_base64 = Image.fromarray(img)
-                img_base64.save(buffered, format="JPEG")
-                encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
-                    'utf-8')  # base64 encoded image with results
-                return render_template('result.html', img_data=encoded_img_data)
-        else:
-            abort(404)
+        # RGB 형식으로 변환
+        img = img.convert("RGB")
+
+        # 변환된 이미지를 사용하여 모델에 전달
+        results = model.predict(source=img, save=True)
+        for result in results:
+            top5_list = result.probs.top5
+
+            # 가져온 데이터 사용 예시
+            if top5_list is not None:
+                # print("Top 5 Classes:", top5_list)
+                model_result = top5_list[0]
+
+        matching_data = get_matching_data(model_result)
+
+        image_directory = 'runs/classify/predict/'
+
+        # 이미지 파일명을 동적으로 얻어오기
+        image_files = os.listdir(image_directory)
+        if not image_files:
+            return "No image files found."
+
+        # 첫 번째 이미지 파일 사용
+        dynamic_image_filename = image_files[0]
+        img_path = os.path.join(image_directory, dynamic_image_filename)
+
+        return render_template('result.html', matching_data=matching_data, img_path=img_path)
+
 
     else:
         return render_template("eye_detect.html")
+# @app.route('/eye_detect', methods=['GET', 'POST'])
+# def eye_detect():
+#     if request.method == 'POST':
+#
+#         im_file = request.files['file']
+#         if im_file != '':
+#             im_bytes = im_file.read()
+#             img = Image.open(io.BytesIO(im_bytes))
+#
+#             results = model(img)  # inference
+#
+#             results.ims  # array of original images (as np array) passed to model for inference
+#             results.render()  # updates results.imgs with boxes and labels
+#             for img in results.ims:  # 'JpegImageFile' -> bytes-like object
+#                 buffered = io.BytesIO()
+#                 img_base64 = Image.fromarray(img)
+#                 img_base64.save(buffered, format="JPEG")
+#                 encoded_img_data = base64.b64encode(buffered.getvalue()).decode(
+#                     'utf-8')  # base64 encoded image with results
+#                 return render_template('result.html', img_data=encoded_img_data)
+#         else:
+#             abort(404)
+#
+#     else:
+#         return render_template("eye_detect.html")
+
 
 @app.route('/bcs')
 def bcs():
     return render_template("bcs.html")
 
+
 @app.route('/hospital')
 def hospital():
-    return render_template("hospital.html",api_keys='e4780035288430c0c50d0571a16174f1', markers=markers)
+    return render_template("hospital.html")
+
 
 @app.route('/dog_bcs')
 def dog_bcs():
     return render_template("dog_bcs.html")
 
+
 @app.route('/cat_bcs')
 def cat_bcs():
     return render_template("cat_bcs.html")
 
+
 @app.route('/bcs_1')
 def bcs_1():
     return render_template("bcs_1.html")
+
+
 @app.route('/bcs_2')
 def bcs_2():
     return render_template("bcs_2.html")
+
+
 @app.route('/bcs_3')
 def bcs_3():
     return render_template("bcs_3.html")
+
+
 @app.route('/bcs_4')
 def bcs_4():
     return render_template("bcs_4.html")
+
+
 @app.route('/bcs_5')
 def bcs_5():
     return render_template("bcs_5.html")
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
 
 
 if __name__ == '__main__':
